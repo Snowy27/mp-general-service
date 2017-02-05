5148deb71ad7bdae8bf9795a39df173f8ce7c926
Snowy27 <mdolt27@gmail.com>
2017-02-05 11:37:41 -0500

    Added validation and params

--- README.md
+++ README.md
@@ -3,16 +3,19 @@ This is the basic service that starts up Hapi server and Seneca client with prov
 *Example of usage:*
 
 ```javascript
-let tutorialService = new Service('tutorial', [{method: 'GET', version: 'v1', cmd: 'help', handler: helpHandle}], {
+let tutorialService = new Service('tutorial', [{method: 'GET', version: 'v1', cmd: 'help', params: ['lesson_id'], handler: helpHandle}], {
     PORT:3000,
     SENECA_LISTEN_PORT: 3001
 });
 ```
 
-This will create a Hapi server on port 3000, with the route GET /tutorial/v1/help
-As well as a Seneca client, that will take port 3001
+This will create a Hapi server that will listen on port 3000, with the route GET /tutorial/v1/{lesson_id}/help,
+as well as a Seneca client, that will take port 3001.
 You can connect to this Seneca client from other services passing SENECA_CLIENT_PORTS: [3001] in the config.
-After that you will be able to call this task with this.seneca.actAsync({service: 'tutorial', version: 'v1', method: 'GET', cmd: 'help'})
+After that you will be able to call this task with
+```javascript
+this.seneca.actAsync({service: 'tutorial', version: 'v1', method: 'GET', cmd: 'help'})
+```
 Both Hapi server and Seneca client retun response in the next form:
 
 ```javascript
@@ -21,3 +24,5 @@ Both Hapi server and Seneca client retun response in the next form:
     response: {...}
 }
 ```
+
+Additionaly, you can pass Joi validation schema to the route {method: 'GET', validation: {SCHEMA}, ...}
--- index.js
+++ index.js
@@ -6,49 +6,57 @@ module.exports = require('./lib');
 //EXAMPLE:
 
 
-// const Service = require('./lib');
-// const BPromise = require('bluebird');
-//
-// let getSong = function getSong (message) {
-//     return BPromise.resolve(message.random / message.notRandom);
-// };
-//
-// let helpHandle = function helpHandle(message) {
-//     return this.seneca.actAsync({
-//             service: 'search',
-//             version: 'v1',
-//             method: 'GET',
-//             cmd: 'song',
-//             random: 2000,
-//             notRandom: 5
-//         })
-//         .then(function(resp) {
-//             return resp.response / 2;
-//         });
-// };
-//
-// let routes = [
-//     {
-//         method: 'GET',
-//         version: 'v1',
-//         cmd: 'song',
-//         handler: getSong
-//     }
-// ];
-//
-// let helperRoutes = [
-//     {
-//         method: 'GET',
-//         version: 'v1',
-//         cmd: 'help',
-//         handler: helpHandle
-//     }
-// ];
-//
-// let search = new Service('search', routes, {
-//     SENECA_LISTEN_PORT: 3001
-// });
-//
+const Service = require('./lib');
+const BPromise = require('bluebird');
+const Joi = require('joi');
+
+let getSong = function getSong (message) {
+    console.log(message);
+    return BPromise.resolve(message.random / message.notRandom);
+};
+
+let helpHandle = function helpHandle(message) {
+    return this.seneca.actAsync({
+            service: 'search',
+            version: 'v1',
+            method: 'GET',
+            cmd: 'song',
+            random: 2000,
+            notRandom: 5
+        })
+        .then(function(resp) {
+            return resp.response / 2;
+        });
+};
+
+let routes = [
+    {
+        method: 'GET',
+        version: 'v1',
+        cmd: 'song',
+        handler: getSong,
+        params: ['song_id'],
+        validation: {
+            random: Joi.number().integer().max(2020),
+            notRandom: Joi.number().integer().required().invalid(0),
+            sond_id: Joi.number()
+        }
+    }
+];
+
+let helperRoutes = [
+    {
+        method: 'GET',
+        version: 'v1',
+        cmd: 'help',
+        handler: helpHandle
+    }
+];
+
+let search = new Service('search', routes, {
+    SENECA_LISTEN_PORT: 3001
+});
+
 // let helper = new Service('helper', helperRoutes, {
 //     PORT:3002,
 //     SENECA_LISTEN_PORT: 3003,
